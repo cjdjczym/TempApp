94cfe467a3534ac26dac42a997901dc7987118f2
cjdjczym <1732018999@qq.com>
2021-09-08 21:53:13 +0800

    首页修改样式（backgroundcolor, appbar...）

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
 import 'dart:io';
-import 'package:flutter/foundation.dart';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:temperature_app/camera_widget.dart';
@@ -9,25 +8,29 @@ import 'package:temperature_app/logic_extension.dart';
 import 'package:temperature_app/ui_extension.dart';
 
 void main() {
-  runZoned<void>(() {
-    WidgetsFlutterBinding.ensureInitialized();
-    runApp(TempApp());
-  }, zoneSpecification: ZoneSpecification(print: (_, __, ___, line) {
-    if (TempApp.logs.length > 30) TempApp.logs.clear();
-    TempApp.logs.add(line);
-  }));
+  runApp(TempApp());
 }
 
 class TempApp extends StatelessWidget {
-  static List<String> logs = List();
+  static double contentScale = 1.5;
+  static double decorationWidth = 4;
   static double screenWidth;
   static double screenHeight;
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider(create: (ctx) => TempNotifier()),
-    ], child: MaterialApp(home: MainPage()));
+    return MaterialApp(
+      builder: (context, child) => GestureDetector(
+          onTapDown: (TapDownDetails details) {
+            FocusScopeNode currentFocus = FocusScope.of(context);
+            if (!currentFocus.hasPrimaryFocus &&
+                currentFocus.focusedChild != null) {
+              FocusManager.instance.primaryFocus.unfocus();
+            }
+          },
+          child: child),
+      home: MainPage(),
+    );
   }
 }
 
@@ -38,47 +41,45 @@ class MainPage extends StatelessWidget {
     TempApp.screenWidth = size.width;
     TempApp.screenHeight = size.height;
     return Scaffold(
-      // body: TempWidget(),
-      body: PageView(
-          scrollDirection: Axis.horizontal,
-          physics: BouncingScrollPhysics(),
-          children: [TempWidget(), CameraMain()]),
+      appBar: AppBar(
+          backgroundColor: Color.fromRGBO(98, 103, 124, 1.0),
+          title: Text("红外测温App"),
+          centerTitle: true,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(top: 3),
+              child: IconButton(
+                icon: Icon(Icons.list, size: 28),
+                splashRadius: 30,
+                onPressed: () {},
+              ),
+            )
+          ]),
+      body: ChangeNotifierProvider<TempNotifier>(
+          create: (ctx) => TempNotifier(), child: TempWidget()),
     );
-    // return CameraWidget();
   }
 }
 
-class TempWidget extends StatefulWidget {
-  @override
-  _TempWidgetState createState() => _TempWidgetState();
-}
-
-class _TempWidgetState extends State<TempWidget> {
-  Socket socket;
-  bool flag = false;
+class TempWidget extends StatelessWidget {
+  final ValueNotifier<bool> showCamera = ValueNotifier(false);
+  static const IP = "192.168.43.2";
   final List<double> bufferList = List();
 
   connect(TempNotifier notifier) async {
-    const IP = "192.168.43.2";
-    socket = await Socket.connect(IP, 80);
-    socket.listen((event) {
-      // print("数据类型: ${event.runtimeType} | 预计数据量: ${event.length / 4}");
+    notifier.socket = await Socket.connect(IP, 80);
+    var flag = false;
+    notifier.socket.listen((event) {
       if (event[0] == 66) {
         flag = true;
       } else if (flag) {
         List<double> list = event.buffer.asFloat32List();
-        // print("本次float数据量： ${list.length}---------------------------------");
         bufferList.addAll(list);
         if (bufferList.length > 768) {
           flag = false;
           bufferList.clear();
         }
         if (bufferList.length == 768) {
-          // print(
-          // "==========================成功接收一组数据=============================");
-          // print(bufferList.toString());
-          // print(
-          //     "======================================================================");
           notifier.dataList = bufferList;
           flag = false;
           bufferList.clear();
@@ -89,28 +90,49 @@ class _TempWidgetState extends State<TempWidget> {
 
   @override
   Widget build(BuildContext context) {
+    var notifier = Provider.of<TempNotifier>(context, listen: false);
     var canvasHeight = TempApp.screenWidth * 512 / 384;
-    return Consumer<TempNotifier>(builder: (context, notifier, _) {
-      return SafeArea(
-        child: DefaultTextStyle(
-          style: TextStyle(fontSize: 12),
+    return SafeArea(
+      child: DefaultTextStyle(
+        style: TextStyle(fontSize: 12),
+        child: Container(
+          color: Color.fromRGBO(98, 103, 124, 1.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Stack(
-                children: [
-                  Container(
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                        color: Colors.blue, width: TempApp.decorationWidth)),
+                child: Stack(
+                  children: [
+                    ValueListenableBuilder(
+                        valueListenable: showCamera,
+                        builder: (_, value, __) {
+                          return SizedBox(
+                            height: canvasHeight,
+                            child: value ? CameraMain() : Container(),
+                          );
+                        }),
+                    Container(
                       height: canvasHeight,
-                      child: notifier.dataList == null
-                          ? Container()
-                          : CustomPaint(
-                              size: Size(TempApp.screenWidth, canvasHeight),
-                              painter: TempPainter(notifier))),
-                  Container(
-                    height: canvasHeight,
-                    child: notifier.showCamera ? CameraMain() : Container(),
-                  )
-                ],
+                      child: Consumer<TempNotifier>(
+                        builder: (context, tempNotifier, _) {
+                          if (notifier.dataList == null) {
+                            return Container();
+                          } else {
+                            return RepaintBoundary(
+                              child: CustomPaint(
+                                  size: Size(TempApp.screenWidth, canvasHeight),
+                                  painter: TempPainter(tempNotifier)),
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
               Container(height: 10),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
@@ -120,65 +142,68 @@ class _TempWidgetState extends State<TempWidget> {
                   child: FlatButton(
                       height: 32,
                       onPressed: () => connect(notifier),
-                      color: Color.fromRGBO(98, 103, 124, 1.0),
+                      color: Colors.green[300],
                       child: Text('start')),
                 ),
-                button('A'),
-                button('B'),
-                button('C')
+                button('A', notifier),
+                button('B', notifier),
+                button('C', notifier)
               ]),
               Container(height: 10),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                button('D'),
-                button('E'),
-                button('P'),
-                button('Q')
+                button('D', notifier),
+                button('E', notifier),
+                button('P', notifier),
+                button('Q', notifier)
               ]),
               Container(height: 10),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                button('伪彩1', () => notifier.colorMaker = pseudo1),
-                button('伪彩2', () => notifier.colorMaker = pseudo2),
-                button('金属1', () => notifier.colorMaker = metal1),
-                button('金属2', () => notifier.colorMaker = metal2)
+                button('彩虹1', notifier, () => notifier.colorMaker = rainbow1),
+                button('彩虹2', notifier, () => notifier.colorMaker = rainbow2),
+                button('彩虹3', notifier, () => notifier.colorMaker = rainbow3),
+                button('灰度', notifier, () => notifier.colorMaker = gray)
               ]),
+              // Container(height: 10),
+              // Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              //   button('伪彩1', notifier, () => notifier.colorMaker = pseudo1),
+              //   button('伪彩2', notifier, () => notifier.colorMaker = pseudo2),
+              //   button('金属1', notifier, () => notifier.colorMaker = metal1),
+              //   button('金属2', notifier, () => notifier.colorMaker = metal2)
+              // ]),
               Container(height: 10),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                button('彩虹1', () => notifier.colorMaker = rainbow1),
-                button('彩虹2', () => notifier.colorMaker = rainbow2),
-                button('彩虹3', () => notifier.colorMaker = rainbow3),
-                button('灰度', () => notifier.colorMaker = gray)
-              ]),
-              Container(height: 10),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                // button('刷新', () => setState(() {})),
-                // button('清除', () => setState(() => TempApp.logs.clear())),
-                button(notifier.showCamera ? '隐藏' : '显示',
-                    () => notifier.showCamera = !notifier.showCamera),
-                button('测试',
+                ValueListenableBuilder(
+                  valueListenable: showCamera,
+                  builder: (_, value, __) {
+                    return SizedBox(
+                        width: 70,
+                        height: 32,
+                        child: FlatButton(
+                            height: 32,
+                            color: Colors.green[300],
+                            onPressed: () {
+                              showCamera.value = !showCamera.value;
+                            },
+                            child: Text(value ? '隐藏' : '显示')));
+                  },
+                ),
+                button('测试', notifier,
                     () => notifier.dataList = test.map((e) => e + 10).toList())
-              ]),
-              // Container(
-              //   height: 70,
-              //   child: ListView.builder(
-              //       padding: EdgeInsets.symmetric(vertical: 3, horizontal: 15),
-              //       itemCount: TempApp.logs.length,
-              //       itemBuilder: (context, index) => Text(TempApp.logs[index],
-              //           style: TextStyle(fontSize: 12, color: Colors.black))),
-              // )
+              ])
             ],
           ),
         ),
-      );
-    });
+      ),
+    );
   }
 
-  Widget button(String abc, [Function fun]) => Container(
+  Widget button(String abc, TempNotifier notifier, [Function fun]) => Container(
         width: 70,
         height: 32,
         child: FlatButton(
             height: 32,
-            onPressed: () => fun == null ? socket.write(abc) : fun(),
-            color: Color.fromRGBO(98, 103, 124, 1.0),
+            onPressed: () => fun == null ? notifier.socket.write(abc) : fun(),
+            color: Colors.green[300],
             child: Text(abc)),
       );
 }
@@ -192,7 +217,7 @@ class TempPainter extends CustomPainter {
         colorMaker = notifier.colorMaker;
 
   @override
-  void paint(Canvas canvas, Size size) {
+  void paint(Canvas canvas, Size size) async {
     Paint paint = Paint()..style = PaintingStyle.fill;
     int x = list.length, y = list[0].length;
     double sliceX = size.width / x, sliceY = size.height / y;
@@ -206,7 +231,19 @@ class TempPainter extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(TempPainter oldDelegate) =>
-      oldDelegate.list != this.list ||
-      oldDelegate.colorMaker != this.colorMaker;
+  bool shouldRepaint(TempPainter oldDelegate) => true;
 }
+
+// class TempPainter extends CustomPainter {
+//   final ui.Image image;
+//
+//   TempPainter(TempNotifier notifier) : image = notifier.image;
+//
+//   @override
+//   void paint(Canvas canvas, Size size) async {
+//     canvas.drawImage(image, Offset.zero, Paint());
+//   }
+//
+//   @override
+//   bool shouldRepaint(TempPainter oldDelegate) => true;
+// }
